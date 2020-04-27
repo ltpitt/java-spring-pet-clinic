dda2331b12441ad16c5ba7fd0ed53225c86264b6
Davide Nastri <d.nastri@gmail.com>
2020-04-27 22:42:03 +0200

    feat: Added Vets index page and controller.

--- java-spring-pet-clinic-web/src/main/java/it/davidenastri/petclinic/controllers/VetController.java
+++ java-spring-pet-clinic-web/src/main/java/it/davidenastri/petclinic/controllers/VetController.java
@@ -1,15 +1,27 @@
 package it.davidenastri.petclinic.controllers;
 
+import it.davidenastri.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
+
     }
 
 }
